481137211e86d98ff418bd5a326f6623eadc48f6
Elise Guezel <elise.guezel@yahoo.fr>
2020-01-30 12:58:57 +0100

    Can retrieve infos reservatiosn

--- src/Components/Panel/Panel.jsx
+++ src/Components/Panel/Panel.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import Ctx_User from '../../Ctx_User';
 
 const Panel = () => {
+  const [user, setUser] = useContext(Ctx_User);
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/reservations/${user.id}`)
+    .then((result) => {
+      console.log(result.data);
+      setReservations(result.data);
+    });
+  }, []);
+
   return (
     <div>
-      You are logged
+      <h3>Welcome {user.email}</h3>
+      { reservations.length > 0 ?
+      <p>Vos reservations</p>
+        :
+      <p>Vous n'avez aucunes réservations</p>
+      }
     </div>
   )
 }
